41aad652c80d9660ce9d4e796d34261b71a02809
Bryan Nunez <bryan.nunez@gmail.com>
2014-10-22 11:38:14 -0400

    cleaned up index.php

--- cs-tutorial/index.php
+++ cs-tutorial/index.php
@@ -8,17 +8,4 @@ $langfile = $lang . ".html";
 		include ("en.html");
 	}
 
-// switch ($lang){
-//     case $lang:
-// 		if(file_exists($langfile))
-// 		echo $langfile;
-//     	//include $langfile;
-//         break;
-//  	default:
-//         echo "PAGE EN - Setting Default";
-//         //include("en.html");
-//         //include EN in all other cases of different lang detection
-//         break;
-
-// }
 ?>
